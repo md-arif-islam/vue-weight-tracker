30b656cf7764996bfd34cb7aad0f9338b38143d7
md-arif-islam <arifislamarif344@gmail.com>
2023-08-14 10:52:59 +0600

    ADDED: - Imported Chart from "chart.js/auto". - Defined variables: widths, widthChartEl, widthChart, widthInput. - Created computed property currentWidth. - Defined function addWidth to add width values to the widths array.
    UPDATED:
    - Refactored currentWidth computed property to sort widths in descending order based on date.
    
    REMOVED:
    - None.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup>
 import { ref, shallowRef, computed, watch, nextTick } from "vue";
 import Chart from "chart.js/auto";
+
+const widths = ref([]);
+const widthChartEl = ref(null);
+const widthChart = shallowRef(null);
+const widthInput = ref(60.0);
+const currentWidth = computed(() => {
+  return widths.value.sort((a, b) => b.date - a.date[0] || { width: 0 });
+});
+const addWidth = () => {
+  widths.value.push({
+    width: widthInput.value,
+    date: new Date().getTime(),
+  });
+};
 </script>
 
 <template>
